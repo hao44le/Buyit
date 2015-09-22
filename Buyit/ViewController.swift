f8cd7d4bac6f6a16dee6187e43681ca197219156
Gelei Chen <chen1474@purdue.edu>
2015-09-22 18:04:15 -0400

    Initial Commit

---
+++ Buyit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Buyit
+//
+//  Created by Gelei Chen on 22/9/2015.
+//  Copyright © 2015 geleichen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
