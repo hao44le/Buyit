9e81ae2bfc3acc67b19dd24a2b925ca91f495872
Gelei Chen <chen1474@purdue.edu>
2015-10-28 10:33:38 -0400

    update icon

--- Buyit/WelcomeViewController.swift
+++ Buyit/WelcomeViewController.swift
@@ -12,55 +12,81 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
     
-    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    @IBOutlet weak var mediumButon: UIButton!
+    @IBOutlet weak var easyButton: UIButton!
+//    @IBOutlet weak var startButton: UIButton!
     
-    @IBAction func startClicked(sender: UIButton) {
-        NSUserDefaults.standardUserDefaults().setInteger(Int(self.timeLabel.text!)!, forKey: "difficulty")
-        self.dismissViewControllerAnimated(true, completion: nil)
+//    @IBAction func startClicked(sender: UIButton) {
+//        NSUserDefaults.standardUserDefaults().setInteger(Int(self.timeLabel.text!)!, forKey: "difficulty")
+//        self.dismissViewControllerAnimated(true, completion: nil)
+//    }
+    
+    @IBAction func easyClicked(sender: UIButton) {
+        NSUserDefaults.standardUserDefaults().setInteger(1, forKey: "difficulty")
+               self.dismissViewControllerAnimated(true, completion: nil)
+        //    }
     }
+    
+    @IBAction func mediumClicked(sender: AnyObject) {
+        NSUserDefaults.standardUserDefaults().setInteger(2, forKey: "difficulty")
+                self.dismissViewControllerAnimated(true, completion: nil)
+        //    }
+    }
+
+    @IBAction func hardClicked(sender: UIButton) {
+        NSUserDefaults.standardUserDefaults().setInteger(3, forKey: "difficulty")
+                self.dismissViewControllerAnimated(true, completion: nil)
+        //    }
+    }
+    
+    
     override func viewDidLoad() {
+        self.hardButton.layer.cornerRadius = 20
+        self.mediumButon.layer.cornerRadius = 20
+        self.easyButton.layer.cornerRadius = 20
         NSUserDefaults.standardUserDefaults().setBool(true, forKey: "welcomeVisited")
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 31/255, green: 61/255, blue: 91/255, alpha: 1)
-        timeLabel.text = "1"
-        startButton.hidden = true
-        startButton.backgroundColor = UIColor(red: 98/255, green: 243/255, blue: 252/255, alpha: 1)
-        startButton.layer.cornerRadius = 35
-        
-        let width = self.view.frame.width
-        let height = self.view.frame.height
-        let hourSliderFrame: CGRect = CGRectMake(width / 2 - 250 , height / 2 - 150, 500, 500)
-        let hourSlider: EFCircularSlider = EFCircularSlider(frame: hourSliderFrame)
-        hourSlider.unfilledColor = UIColor(red:23 / 255.0, green: 47 / 255.0, blue: 70 / 255.0, alpha: 1.0)
-        hourSlider.filledColor = UIColor(red: 98/255, green: 243/255, blue: 252/255, alpha: 1)
-        
-        hourSlider.innerMarkingLabels = (["2", "3", "1"])
-        hourSlider.labelFont = UIFont.systemFontOfSize(40.0)
-        hourSlider.lineWidth = 20
-        hourSlider.snapToLabels = false
-        hourSlider.minimumValue = -1
-        hourSlider.maximumValue = 3
-        hourSlider.labelColor = UIColor(red:127 / 255.0, green: 229 / 255.0, blue: 255 / 255.0, alpha: 1.0)
-        hourSlider.handleType = CircularSliderHandleTypeDoubleCircleWithClosedCenter
-        
-        hourSlider.handleColor = hourSlider.filledColor;
-        self.view.addSubview(hourSlider)
-        self.view.sendSubviewToBack(hourSlider)
-        hourSlider.addTarget(self, action: "hourDidChange:", forControlEvents: UIControlEvents.ValueChanged)
+
+//        startButton.hidden = true
+//        startButton.backgroundColor = UIColor(red: 98/255, green: 243/255, blue: 252/255, alpha: 1)
+//        startButton.layer.cornerRadius = 35
+//        
+//        let width = self.view.frame.width
+//        let height = self.view.frame.height
+//        let hourSliderFrame: CGRect = CGRectMake(width / 2 - 250 , height / 2 - 150, 500, 500)
+//        let hourSlider: EFCircularSlider = EFCircularSlider(frame: hourSliderFrame)
+//        hourSlider.unfilledColor = UIColor(red:23 / 255.0, green: 47 / 255.0, blue: 70 / 255.0, alpha: 1.0)
+//        hourSlider.filledColor = UIColor(red: 98/255, green: 243/255, blue: 252/255, alpha: 1)
+//        
+//        hourSlider.innerMarkingLabels = (["2", "3", "1"])
+//        hourSlider.labelFont = UIFont.systemFontOfSize(40.0)
+//        hourSlider.lineWidth = 20
+//        hourSlider.snapToLabels = false
+//        hourSlider.minimumValue = -1
+//        hourSlider.maximumValue = 3
+//        hourSlider.labelColor = UIColor(red:127 / 255.0, green: 229 / 255.0, blue: 255 / 255.0, alpha: 1.0)
+//        hourSlider.handleType = CircularSliderHandleTypeDoubleCircleWithClosedCenter
+//        
+//        hourSlider.handleColor = hourSlider.filledColor;
+//        self.view.addSubview(hourSlider)
+//        self.view.sendSubviewToBack(hourSlider)
+//        hourSlider.addTarget(self, action: "hourDidChange:", forControlEvents: UIControlEvents.ValueChanged)
         // Do any additional setup after loading the view.
     }
     
-    func hourDidChange(slider: EFCircularSlider) {
-        var value = Int(slider.currentValue + 0.5)
-        if value == 0 {
-            value = 1
-        }
-        if value == 4 {
-            value = 3
-        }
-        startButton.hidden = false
-        timeLabel.text = NSString(format: "%d", value) as String
-    }
+//    func hourDidChange(slider: EFCircularSlider) {
+//        var value = Int(slider.currentValue + 0.5)
+//        if value == 0 {
+//            value = 1
+//        }
+//        if value == 4 {
+//            value = 3
+//        }
+//        startButton.hidden = false
+        //timeLabel.text = NSString(format: "%d", value) as String
+    //}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
